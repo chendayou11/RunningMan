6d48b8866205cd81ed1e4ce6e11d390bb264ff02
chendayou11 <144404481@qq.com>
2015-08-04 11:21:43 +0800

    move
    move how to fix

--- src/app/views/GameMap.lua
+++ src/app/views/GameMap.lua
@@ -5,12 +5,26 @@
 -- Time: 11:37
 -- To change this template use File | Settings | File Templates.
 --
-local _tileMap
 local GameMap=class("GameMap",cc.Layer)
 function GameMap:ctor()
-    local _tileMap=cc.TMXTiledMap:create("map00.tmx")
-    self:addChild(_tileMap,0,100)
-
+    local Map_1=cc.TMXTiledMap:create("map00.tmx")
+    self:addChild(Map_1,0,100)
+    local Map_2=cc.TMXTiledMap:create("map01.tmx")
+    Map_2:setPositionX(display.width)
+    print(Map_1:getMapSize().width)
+    self:addChild(Map_2,0,100)
+    self.Map_1=Map_1
+    self.Map_2=Map_2
+    local timer=cc.Director:getInstance():getScheduler():scheduleScriptFunc(handler(self,self.bg),1,false)
+end
+function GameMap:bg()
+    self.Map_1:setPositionX(self.Map_1:getMapSize().width-0.5)
+    self.Map_2:setPositionX(self.Map_1:getMapSize().width+display.width-0.5)
+    print("fasljf;asjf;lsd")
+    if self.Map_1:getPositionX()==-self.Map_1:getMapSize().width then
+        print("123123")
+        self.Map_1:setPositionX(0)
+    end
 end
 return GameMap
 
